11169a74079372520d3be3c3beefda0b83cd10de
tomomiK <t0m0m1k221@gmail.com>
2023-09-05 11:31:24 +0900

    feat:CRUD機能を実装した

--- my-app/app/api/todo/route.tsx
+++ my-app/app/api/todo/route.tsx
@@ -5,3 +5,37 @@ export async function GET() {
   const todos = await prisma.todo.findMany();
   return NextResponse.json(todos);
 }
+
+export async function POST(request: Request) {
+  const req = await request.json();
+  await prisma.todo.create({ data: req });
+
+  return NextResponse.json(req);
+}
+
+export async function DELETE(request: Request) {
+  const { id } = await request.json();
+
+  await prisma.todo.delete({
+    where: {
+      todo_id: id, 
+    },
+  });
+  return NextResponse.json({ message: 'ToDo deleted successfully' });
+}
+
+export async function PUT(request: Request) {
+  const { id, title, category } = await request.json();
+
+  const updatedTodo = await prisma.todo.update({
+    where: {
+      todo_id: id,
+    },
+    data: {
+      todo_title: title,
+      todo_category: category,
+    },
+  });
+
+  return NextResponse.json(updatedTodo);
+}
