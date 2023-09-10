df64eeba18e4ec88ba225639aa787f0a5eb2f7f8
tomomiK <t0m0m1k221@gmail.com>
2023-09-10 20:29:47 +0900

    feat:完了ボタンクリック時と全削除ボタンクリック時にアラートが出現する機能を実装した`

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { todo } from '@prisma/client';
+import { handleCompleteAndDelete } from './api/todo_api';
 import { useRouter } from 'next/navigation';
 import { 
   AlertDialog,
@@ -22,9 +23,11 @@ import {
   useDisclosure,
 } from '@chakra-ui/react';
 import { fetchTodos } from './api/todo_api'; 
-import { useState, useEffect } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 const Page = () => {
+
+  type FocusableElement = any
 
   const router = useRouter();
 
@@ -48,6 +51,12 @@ const Page = () => {
     router.push("/todo_edit");
   }
 
+  const { isOpen: isCompleteDialogOpen, onOpen: onCompleteDialogOpen, onClose: onCompleteDialogClose } = useDisclosure();
+  const completeDialogCancelRef = useRef<FocusableElement | null>(null);
+
+  const { isOpen: isDeleteDialogOpen, onOpen: onDeleteDialogOpen, onClose: onDeleteDialogClose } = useDisclosure();
+  const deleteDialogCancelRef = useRef<FocusableElement | null>(null);
+  
   return (
     <Box m={6}>
       <Heading as="h1" size="lg" fontWeight="bold">
@@ -75,16 +84,82 @@ const Page = () => {
                 <Td>
                   <Button onClick={() => handleDetailClick(todo.todo_id)}>詳細</Button>
                   <Button onClick={handleEditClick}>編集</Button>
-                  <Button>完了</Button>
+                  <>
+                    <Button colorScheme='red' onClick={onCompleteDialogOpen}>
+                      完了
+                    </Button>
+
+                    <AlertDialog
+                      isOpen={isCompleteDialogOpen}
+                      leastDestructiveRef={completeDialogCancelRef}
+                      onClose={onCompleteDialogClose}
+                    >
+                      <AlertDialogOverlay>
+                        <AlertDialogContent>
+                          <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+                            完了
+                          </AlertDialogHeader>
+
+                          <AlertDialogBody>
+                            完了したためTODOを削除しますがよろしいですか？
+                          </AlertDialogBody>
+
+                          <AlertDialogFooter>
+                            <Button ref={completeDialogCancelRef} onClick={onCompleteDialogClose}>
+                              キャンセル
+                            </Button>
+                            <Button 
+                              colorScheme='red' 
+                              onClick={() => {
+                                onCompleteDialogClose(); 
+                                handleCompleteAndDelete(todo.todo_id); 
+                              }}
+                              ml={3}>
+                                完了し削除する
+                            </Button>
+                          </AlertDialogFooter>
+                        </AlertDialogContent>
+                      </AlertDialogOverlay>
+                    </AlertDialog>
+                  </>
                 </Td>
               </Tr>
             ))}
           </Tbody>
         </Table>
       </TableContainer>
-      <Button>
+      <>
+      <Button colorScheme='red' onClick={onDeleteDialogOpen}>
         全削除
       </Button>
+
+      <AlertDialog
+        isOpen={isDeleteDialogOpen}
+        leastDestructiveRef={deleteDialogCancelRef}
+        onClose={onDeleteDialogClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+              全削除
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              全てのTODOを削除しますがよろしいですか？
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={deleteDialogCancelRef} onClick={onDeleteDialogClose}>
+                キャンセル
+              </Button>
+              <Button colorScheme='red' onClick={onDeleteDialogClose} ml={3}>
+                削除
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
     </Box>
   );
 };
