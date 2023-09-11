a5393a53c4eaa6b297b000902a1e83a248c30d2c
tomomiK <t0m0m1k221@gmail.com>
2023-09-11 11:01:27 +0900

    feat:詳細ページを実装した

--- my-app/app/todo_detail/[todo_id]/page.tsx
+++ my-app/app/todo_detail/[todo_id]/page.tsx
@@ -1,11 +1,74 @@
-import React from 'react';
+'use client';
 
-function HomePage() {
+import { todo } from '@prisma/client';
+import { useRouter } from 'next/navigation';
+import { 
+  Box, 
+  Button,
+  Heading, 
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+} from '@chakra-ui/react';
+import { fetchTodos } from '../../api/todo_api'; 
+import { useEffect, useState } from 'react';
+
+const Page = ({ params }: { params: { todo_id: string} }) => {
+  const router = useRouter();
+
+  const [todoToShow, setTodoToShow] = useState<todo | null>(null);
+  const todo_id = params.todo_id;
+
+  useEffect(() => {
+    fetchTodos().then((data) => {
+      const selectedTodo = data.find((item) => item.todo_id === Number(todo_id));
+      if (selectedTodo) {
+        setTodoToShow(selectedTodo);
+      }
+    });
+  }, [todo_id, router]);
+
+  const handleListClick = () => {
+    router.push("/");
+  }
+  
   return (
-    <div>
-      <h1>This is Detail Page.</h1>
-    </div>
+    <Box m={6}>
+      <Heading as="h1" size="lg" fontWeight="bold">
+        TODO詳細
+      </Heading>
+      <TableContainer>
+        <Table variant='simple'>
+          <Thead>
+            <Tr>
+              <Th>TODO</Th>
+              <Th>CATEGORY</Th>
+              <Th>ID</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {todoToShow ? (
+                <Tr>
+                  <Td>{todoToShow.todo_title}</Td>
+                  <Td>{todoToShow.todo_category}</Td>
+                  <Td>{todoToShow.todo_id}</Td>
+                </Tr>
+            ) : (
+              <p>TODOが見つかりませんでした。</p>
+            )}
+          </Tbody>
+        </Table>
+      </TableContainer>
+      <Button colorScheme='blue' onClick={handleListClick}>
+        リストに戻る
+      </Button>
+      
+    </Box>
   );
-}
+};
 
-export default HomePage;
+export default Page;
