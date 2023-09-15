c68f74355cbc09792d0ab097209fde05dd2a4f14
tomomiK <t0m0m1k221@gmail.com>
2023-09-15 11:25:51 +0900

    fix: DELETEした際に一覧が再読み込みされないエラーを解消しました

--- my-app/app/api/todo_api.ts
+++ my-app/app/api/todo_api.ts
@@ -1,38 +1,28 @@
 import { todo } from '@prisma/client';
-import { useState } from 'react';
 
 export const fetchTodos = async (): Promise<todo[]> => {
-  const response = await fetch('http://localhost:3000/api/todo');
+  const response = await fetch('http://localhost:3000/api/get_all');
   const todos: todo[] = await response.json();
   return todos;
 };
 
-export const handleCompleteAndDelete = async (todo_id: number) => {
+export const handleCompleteAndDelete = async (todo_id: number, setTodos: React.Dispatch<React.SetStateAction<todo[]>>) => {
   try {
-    // DELETE リクエストを送信
-    const response = await fetch('http://localhost:3000/api/todo', {
+    const response = await fetch(`http://localhost:3000/api/todo/${todo_id}`, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ id: todo_id }),
     });
 
     if (response.ok) {
-      console.log("削除しました")
-      const [todos, setTodos] = useState<todo[]>([]); 
-      const updatedTodos = todos.filter((todo: { todo_id: any; }) => todo.todo_id !== todo_id);
-      setTodos(updatedTodos);
-
-      const refreshedTodos = await fetchTodos();
-      setTodos(refreshedTodos);
-
+      console.log("削除しました");
+      setTodos((prevTodos) => prevTodos.filter((todo) => todo.todo_id !== todo_id));
     } else {
-      // エラーハンドリング
       console.error('ToDoの削除に失敗しました。');
     }
   } catch (error) {
-    // エラーハンドリング
     console.error('エラーが発生しました: ', error);
   }
 };
+
